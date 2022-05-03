ed9b2a78e88b3fab52dfc300a253463fd259c385
Tomáš Chochola <chocholatom1997@gmail.com>
2022-05-03 10:38:36 +0200

    Use TestingHelpersTraits

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tomchochola\Laratchi\Testing\TestingHelpersTraits;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use TestingHelpersTraits;
 }
