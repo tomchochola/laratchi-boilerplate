2201f4bec2988140f66b2d61a143ed3e643db25d
Tomáš Chochola <chocholatom1997@gmail.com>
2022-05-13 16:46:13 +0200

    Storage fake when running unit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Storage;
 use Tomchochola\Laratchi\Testing\TestingHelpersTraits;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use TestingHelpersTraits;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::fake('public');
+    }
 }
