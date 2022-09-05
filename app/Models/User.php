2630d45278c2e9dcfc82e0bce27ad5af15d52adf
Tomáš Chochola <chocholatom1997@gmail.com>
2022-09-05 11:45:36 +0200

    Update model stub

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,101 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Tomchochola\Laratchi\Auth\User as BaseUser;
 
 class User extends BaseUser
 {
+    /**
+     * @inheritDoc
+     *
+     * @var array<mixed>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * What is loaded on index.
+     *
+     * @return array<mixed>
+     */
+    public static function loadIndex(): array
+    {
+        return [];
+    }
+
+    /**
+     * What is selected on index.
+     *
+     * @return array<mixed>
+     */
+    public static function selectIndex(): array
+    {
+        return ['users.id'];
+    }
+
+    /**
+     * What is loaded on detail.
+     *
+     * @return array<mixed>
+     */
+    public static function loadDetail(): array
+    {
+        return [];
+    }
+
+    /**
+     * What is selected on detail.
+     *
+     * @return array<mixed>
+     */
+    public static function selectDetail(): array
+    {
+        return ['users.id'];
+    }
+
+    /**
+     * What is loaded on me.
+     *
+     * @return array<mixed>
+     */
+    public static function loadMe(): array
+    {
+        return [];
+    }
+
+    /**
+     * What is selected on me.
+     *
+     * @return array<mixed>
+     */
+    public static function selectMe(): array
+    {
+        return ['*'];
+    }
+
+    /**
+     * Modify index query.
+     */
+    public static function queryIndex(Builder $builder): void
+    {
+        $builder->with(static::loadIndex())->getQuery()->select(static::selectIndex());
+    }
+
+    /**
+     * Modify detail query.
+     */
+    public static function queryDetail(Builder $builder): void
+    {
+        $builder->with(static::loadDetail())->getQuery()->select(static::selectDetail());
+    }
+
+    /**
+     * Modify me query.
+     */
+    public static function queryMe(Builder $builder): void
+    {
+        $builder->with(static::loadMe())->getQuery()->select(static::selectMe());
+    }
 }
