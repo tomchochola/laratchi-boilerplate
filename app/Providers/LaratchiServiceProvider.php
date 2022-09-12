cd13d370365c90c145ac65bc01dfc0b1dc807aad
Tomáš Chochola <chocholatom1997@gmail.com>
2022-09-12 11:46:51 +0200

    Remove unused namespace

--- app/Providers/LaratchiServiceProvider.php
+++ app/Providers/LaratchiServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Tomchochola\Laratchi\Providers\LaratchiServiceProvider as LaratchiLaratchiServiceProvider;
-use Tomchochola\Laratchi\Validation\SecureValidator;
 
 class LaratchiServiceProvider extends LaratchiLaratchiServiceProvider
 {
