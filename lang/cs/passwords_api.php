b33b409279dad7ba640004c53caa30f0695124e0
Tomáš Chochola <chocholatom1997@gmail.com>
2022-09-13 11:05:04 +0200

    Add api plain validation errors messages

---
+++ lang/cs/passwords_api.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'passwords_reset',
+    'sent' => 'passwords_sent',
+    'throttled' => 'passwords_throttled',
+    'token' => 'passwords_token',
+    'user' => 'passwords_user',
+];
