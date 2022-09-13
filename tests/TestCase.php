903ab84eb555d0aa250730f84899a0b0cb3e3a5c
Tomáš Chochola <chocholatom1997@gmail.com>
2022-09-13 13:40:27 +0200

    Add base user and database token json structure to tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Support\Arr;
 use Tomchochola\Laratchi\Testing\TestCase as LaratchiTestCase;
 
 abstract class TestCase extends LaratchiTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @inheritDoc
+     */
+    protected $defaultHeaders = [
+        'Accept-Language' => 'cs',
+    ];
 
     /**
      * @inheritDoc
@@ -19,9 +27,44 @@ abstract class TestCase extends LaratchiTestCase
             'cs' => [
                 'cs',
             ],
-            'en' => [
-                'en',
-            ],
         ];
+    }
+
+    /**
+     * Me json structure resource.
+     *
+     * @return array<mixed>
+     */
+    protected function jsonStructureMe(?bool $includeDatabaseToken): array
+    {
+        $structure = $this->jsonStructureResource([
+            'email',
+            'name',
+            'locale',
+            'email_verified_at',
+        ]);
+
+        if ($includeDatabaseToken !== false) {
+            Arr::set($structure, 'relationships.database_token', $includeDatabaseToken === null ? ['data' => null] : $this->jsonStructureRelationship());
+        }
+
+        return $structure;
+    }
+
+    /**
+     * Database token json structure resource.
+     *
+     * @return array<mixed>
+     */
+    protected function jsonStructureDatabaseToken(bool $includeBearer): array
+    {
+        return $this->jsonStructureResource($includeBearer ? [
+            'auth_id',
+            'provider',
+            'bearer',
+        ] : [
+            'auth_id',
+            'provider',
+        ]);
     }
 }
