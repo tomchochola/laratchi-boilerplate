03765fee79d93a8b36bf133e9e0bf9c18e547c7a
Tomáš Chochola <chocholatom1997@gmail.com>
2023-01-03 11:10:20 +0100

    Register api routes explicit

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,18 @@ declare(strict_types=1);
 
 resolveRouteRegistrar()->prefix('v1')->group(static function (): void {
     resolveRouteRegistrar()->prefix('auth')->group(static function (): void {
-        Tomchochola\Laratchi\Support\ServiceProvider::registerRoutes();
+        resolveRouter()->post('login', Tomchochola\Laratchi\Auth\Http\Controllers\LoginController::class);
+        resolveRouter()->post('register', Tomchochola\Laratchi\Auth\Http\Controllers\RegisterController::class);
+        resolveRouter()->post('password/forgot', Tomchochola\Laratchi\Auth\Http\Controllers\PasswordForgotController::class);
+        resolveRouter()->post('password/reset', Tomchochola\Laratchi\Auth\Http\Controllers\PasswordResetController::class);
+        resolveRouter()->post('password/update', Tomchochola\Laratchi\Auth\Http\Controllers\PasswordUpdateController::class);
+        resolveRouter()->post('email_verification/resend', Tomchochola\Laratchi\Auth\Http\Controllers\EmailVerificationResendController::class);
+        resolveRouter()->post('email_verification/verify', Tomchochola\Laratchi\Auth\Http\Controllers\EmailVerificationVerifyController::class);
+        resolveRouter()->post('logout/current', Tomchochola\Laratchi\Auth\Http\Controllers\LogoutCurrentDeviceController::class);
+        resolveRouter()->post('logout/other', Tomchochola\Laratchi\Auth\Http\Controllers\LogoutOtherDevicesController::class);
+        resolveRouter()->get('me', Tomchochola\Laratchi\Auth\Http\Controllers\MeShowController::class);
+        resolveRouter()->post('me/destroy', Tomchochola\Laratchi\Auth\Http\Controllers\MeDestroyController::class);
+        resolveRouter()->post('me/update', Tomchochola\Laratchi\Auth\Http\Controllers\MeUpdateController::class);
     });
 });
 
