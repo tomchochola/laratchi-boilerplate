3f27301353be1d15e7e486e470dc514eabded315
Tomáš Chochola <chocholatom1997@gmail.com>
2023-01-05 00:20:52 +0100

    Add more tips

--- app/Providers/LaratchiServiceProvider.php
+++ app/Providers/LaratchiServiceProvider.php
@@ -26,6 +26,8 @@ class LaratchiServiceProvider extends LaratchiLaratchiServiceProvider
         // Tip to $this->app->bind() Tomchochola\Laratchi\Auth\Actions\CanLoginAction::class to global authorize authed users
         // Tip to $this->app->bind() Tomchochola\Laratchi\Auth\Http\Validation\AuthValidity::class to modify auth routes validation
         // Tip to $this->app->bind() Tomchochola\Laratchi\Auth\DatabaseToken::class to modify database token functions
+        // Tip to $this->app->bind() Tomchochola\Laratchi\Auth\Actions\*::class to modify auth system actions
+        // Tip to $this->app->bind() Tomchochola\Laratchi\Auth\Http\Requests\*::class to modify auth system data validation
 
         // Review static::class static properties
         // Review Tomchochola\Laratchi\Auth\Http\Controllers\*::class static properties
@@ -34,5 +36,7 @@ class LaratchiServiceProvider extends LaratchiLaratchiServiceProvider
         // Review Tomchochola\Laratchi\Validation\ValidatedInput::class static properties
 
         // See nice validation rules Tomchochola\Laratchi\Validation\Rules\*::class and their static properties
+        // See nice middlewares Tomchochola\Laratchi\Http\Middleware\*::class
+        // See nice image fakery Tomchochola\Laratchi\Testing\Support\MediaSeedSupport::class
     }
 }
