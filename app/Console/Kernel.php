73cc7b9233b03b545932bfe9b410cb4641b59a49
Tomáš Chochola <chocholatom1997@gmail.com>
2023-01-14 00:34:44 +0100

    Run scheduler in Europe/Prague timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,5 +8,5 @@ use Tomchochola\Laratchi\Console\Kernel as LaratchiKernel;
 
 class Kernel extends LaratchiKernel
 {
-    public const SCHEDULE_TIMEZONE = 'UTC';
+    public const SCHEDULE_TIMEZONE = 'Europe/Prague';
 }
