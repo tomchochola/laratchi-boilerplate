b69250340fda6319bd5fb27455bdc855d6975272
Tomáš Chochola <chocholatom1997@gmail.com>
2023-04-11 15:05:51 +0200

    Add user migration and seeder

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        resolveSchema()->create('users', static function (Blueprint $table): void {
+            $table->id();
+
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken();
+            $table->char('locale', 2);
+
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (isEnv(['staging', 'production'])) {
+            return;
+        }
+
+        if (User::query()->getQuery()->exists()) {
+            return;
+        }
+
+        UserFactory::new()->locale('cs')->password()->blankRememberToken()->unverified()->createOne(['email' => 'testovaci@uzivatel.cz', 'name' => 'Testovací Uživatel']);
+    }
+}
